9b6972a69155e5906cbc5624ed3308dc89a7aa08
Lee Bowyer <cubes@bowyer.fr>
2024-03-13 20:09:45 +0100

    trials

---
+++ controllers/json.js
@@ -0,0 +1,25 @@
+const playground = (req, res, next) => {
+    res.render("playground")
+}
+
+const playgroundv2 = (req, res, next) => {
+    res.render("playgroundv2")
+}
+
+const playgroundv3 = (req, res, next) => {
+    res.render("playgroundv3")
+}
+
+const playgroundv4 = (req, res, next) => {
+    res.render("playgroundv4")
+}
+
+const playgroundv5 = (req, res, next) => {
+    res.render("playgroundv5")
+}
+
+const playgroundv6 = (req, res, next) => {
+    res.render("playgroundv6")
+}
+
+module.exports = {playground, playgroundv2, playgroundv3, playgroundv4, playgroundv5, playgroundv6}
---
+++ end-points/json.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+// Middleware to Sanitise URL params
+const {add_params_to_res_locals} = require("../modules/sanitise_tools")
+
+// Controller for Matter
+const controller = require("../controllers/json")
+
+// Sanitise Parameters and add to res.locals.sanitised_parameters
+
+// Route Controllers
+
+router.get("/", controller.playground)
+router.get("/v2", controller.playgroundv2)
+router.get("/v3", controller.playgroundv3)
+router.get("/v4", controller.playgroundv4)
+router.get("/v5", controller.playgroundv5)
+router.get("/v6", controller.playgroundv6)
+
+module.exports = router
