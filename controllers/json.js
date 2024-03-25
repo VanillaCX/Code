19b0addc89e45725f4687e9cfcc2ac49d31ef2e3
Lee Bowyer <cubes@bowyer.fr>
2024-03-25 08:54:51 +0100

    laptop

--- controllers/json.js
+++ controllers/json.js
@@ -22,4 +22,20 @@ const playgroundv6 = (req, res, next) => {
     res.render("playgroundv6")
 }
 
-module.exports = {playground, playgroundv2, playgroundv3, playgroundv4, playgroundv5, playgroundv6}
+const playgroundv7 = (req, res, next) => {
+    res.render("playgroundv7")
+}
+
+const playgroundv8 = (req, res, next) => {
+    res.render("playgroundv8")
+}
+
+const playgroundv9 = (req, res, next) => {
+    res.render("playgroundv9")
+}
+
+const playgroundv10 = (req, res, next) => {
+    res.render("playgroundv10")
+}
+
+module.exports = {playground, playgroundv2, playgroundv3, playgroundv4, playgroundv5, playgroundv6, playgroundv7, playgroundv8, playgroundv9, playgroundv10}
--- end-points/json.js
+++ end-points/json.js
@@ -17,5 +17,10 @@ router.get("/v3", controller.playgroundv3)
 router.get("/v4", controller.playgroundv4)
 router.get("/v5", controller.playgroundv5)
 router.get("/v6", controller.playgroundv6)
+router.get("/v7", controller.playgroundv7) // STAGE
+router.get("/v8", controller.playgroundv8) // TOO COMPLICATED
+router.get("/v9", controller.playgroundv9) // BACK TO V8
+router.get("/v10", controller.playgroundv10) // BACK TO V8
+
 
 module.exports = router
